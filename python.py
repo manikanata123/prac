bfe1bbdac29a4a0ea06d42676cf3fa56493b8165
manikanata123 <59291117+manikanata123@users.noreply.github.com>
2019-12-28 09:43:52 +0530

    code complted

--- python.py
+++ python.py
@@ -1,6 +1,20 @@
 class A():
  def MyMethod(self):
-   print("Iam in Class A")
+  print("Iam in Class A")
 class B():
   def MyMrthod(self):
-    print("Iam in Class A")
+   print("Iam in Class B")
+class C(B,A):
+ def MyMethod(self):
+  print("I am Class C")
+ 
+def main():
+ obj=C()
+ obj.MyMethod()
+ 
+if __name__ = "__main__":
+ main()
+ 
+ 
+ 
+#Thanks :)
